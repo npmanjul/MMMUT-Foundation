2c4573b6ca41836a3643fda86a3d340717b9a898
AdityaGaur7 <adgaur027@gmail.com>
2024-05-16 19:59:53 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Nav from './common/Nav';
+import Home from './pages/Home';
+import Startup from './pages/Startups';
+import About from './pages/About';
+import Apply from './pages/Apply';
+import Contact from './pages/Contact';
+import Foot from './common/Foot'
 function App() {
-
   return (
-    <>
-      Hello ji
-    </>
-  )
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/startup" element={<Startup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/apply" element={<Apply />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Foot/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home_carousal.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./style.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="/img/homepagepic1.png" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/homepagepic2.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/homepagepic3.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/homepagepic4.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/homepagepic6.jpeg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/homepagepic7.jpeg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/homepagepic8.jpeg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
