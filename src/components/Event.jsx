60069275b04abd435960a0e8843793deff5bbc26
AdityaGaur7 <adgaur027@gmail.com>
2024-06-05 16:00:27 +0530

    added favicon

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import Slider from 'react-slick';
+import React from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './event.css'; // Import custom styles if needed
+import "./event.css"; // Import custom styles if needed
 
 const images = [
   "/img/photo1.jpg",
-  "/img/photo5.jpg",
   "/img/photo2.jpg",
   "/img/photo3.jpg",
   "/img/photo4.jpg",
+  "/img/photo5.jpg",
   "/img/photo6.jpg",
   "/img/photo7.jpg",
   "/img/photo8.jpg",
-  "/img/photo9.jpg",
   "/img/photo11.jpg",
   "/img/photo13.jpg",
   "/img/photo14.jpg",
@@ -29,7 +28,7 @@ const Carousel = () => {
     autoplay: true,
     autoplaySpeed: 2000,
     centerMode: true,
-    centerPadding: '20px',
+    centerPadding: "20px",
     beforeChange: (current, next) => setCurrentIndex(next),
   };
 
@@ -43,10 +42,14 @@ const Carousel = () => {
           <div
             key={index}
             className={`carousel-image-container ${
-              currentIndex === index ? 'active' : 'inactive'
+              currentIndex === index ? "active" : "inactive"
             }`}
           >
-            <img src={image} alt={`Slide ${index}`} className="carousel-image" />
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className="carousel-image"
+            />
           </div>
         ))}
       </Slider>
