310589dabf8c6e8cea58bebb2bf2feed692182ed
anjulsingh999 <anjulsingh462@gmail.com>
2024-06-14 15:04:22 +0530

    fixed some bugs

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -4,8 +4,7 @@ import ContactForm from '../components/ContactForm'
 const Apply = () => {
   return (
     <>
-     {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfdiIN1W2zL7aHTyrSIotW6ZFk_zctJVh-fL4xyVj5J27CMqQ/viewform?embedded=true" width="640" height="300" frameborder="0" marginheight="0" marginwidth="0"></iframe> */}
-     Apply form
+     Apply form for startups
     </>
   )
 }
